f28dcedab441c5248401793785217c32fe3ec312
zeloras <zeloras@gmail.com>
2019-12-11 04:22:04 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import numpy
 
 
-def sigmoid(i):
-    return 1 / (1 + numpy.exp(-i))
+class NeuronNet:
+    iterations: int = 1000
+    training: []
+    outputs: []
+    compare: []
 
+    def __init__(self, input_array: [] = [], output_array: [] = [], to_calculate: [] = []):
+        self.training = input_array
+        self.outputs = output_array
+        self.compare = to_calculate
 
-def calculate(training: [], outputs: [], compare: [], iterations: int = 10000):
-    training = numpy.array(training)
-    outputs = numpy.array([outputs]).T
-    weights = 2 * numpy.random.random((len(training[0]), 1)) - 1
+    @staticmethod
+    def sigmoid(i):
+        return 1 / (1 + numpy.exp(-i))
 
-    for i in range(iterations):
-        outputs_data = sigmoid(numpy.dot(training, weights))
-        error = outputs - outputs_data
-        weights += numpy.dot(training.T, error * (outputs_data * (1 - outputs_data)))
+    def calculate(self):
+        training = numpy.array(self.training)
+        outputs = numpy.array([self.outputs]).T
+        weights = 2 * numpy.random.random((len(training[0]), 1)) - 1
 
-    return sigmoid(numpy.dot(numpy.array(compare), weights))
+        for i in range(self.iterations):
+            outputs_data = self.sigmoid(numpy.dot(training, weights))
+            error = outputs - outputs_data
+            weights += numpy.dot(training.T, error * (outputs_data * (1 - outputs_data)))
+
+        return self.sigmoid(numpy.dot(numpy.array(self.compare), weights))
 
 
 out_truth_data = [1, 0, 1, 0, 0, 1]
@@ -26,4 +37,6 @@ training_data = [[0, 1, 1, 1],
             [0, 0, 0, 0],
             [0, 1, 1, 1]]
 compare_data = [0, 0, 1, 0]
-print(calculate(training_data, out_truth_data, compare_data, iterations=1000))
+
+nn = NeuronNet(training_data, out_truth_data, compare_data)
+print(nn.calculate())
